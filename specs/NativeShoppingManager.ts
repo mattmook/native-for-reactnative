be8f5be49b0733d255479449b9dd441ccbe43c04
Matthew Dolan <info@mattdolan.com>
2024-11-06 19:47:44 +0000

    Remove spec

--- specs/NativeShoppingManager.ts
+++
@@ -1,9 +0,0 @@
-import type {TurboModule} from 'react-native';
-import {TurboModuleRegistry} from 'react-native';
-
-export interface Spec extends TurboModule {
-  getNativeTotalPrice(): string;
-  getNativeTotalPriceAsync(): Promise<string>;
-}
-
-export default TurboModuleRegistry.get<Spec>("NativeShoppingManager") as Spec | null;
